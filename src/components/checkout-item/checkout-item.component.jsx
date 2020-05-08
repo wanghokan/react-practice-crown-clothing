0ad12bfaea309de38bcbee3702f8315474f37c6d
unknown <w52806991@gmail.com>
2020-05-08 22:58:43 +0800

    added addItem and removeItem functionality to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
+import { addItem, removeItem, clearItemFromCart } from '../../redux/cart/cart.actions'
 
 import './checkout-item.style.scss'
 
-const CheckoutItem = ({ cartItem: { imageUrl, name, quantity, price } }) => (
-    <div className='checkout-item'>
-        <div className='image-container'>
-            <img src={imageUrl} alt='item' />
-        </div>
-        <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
-        <span className='price'>{price}</span>
-        <div className='remove-button'>&#10005;</div>
-    </div>
-)
+const CheckoutItem = ({ cartItem, addItem, removeItem, clearItem }) => {
+    const { imageUrl, name, quantity, price } = cartItem
 
-export default CheckoutItem
+    return (
+        <div className='checkout-item'>
+            <div className='image-container'>
+                <img src={imageUrl} alt='item' />
+            </div>
+            <span className='name'>{name}</span>
+            <span className='quantity'>
+                <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
+                <span className='value'>{quantity}</span>
+                <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
+            </span>
+            <span className='price'>{price}</span>
+            <div className='remove-button' onClick={() => clearItem(cartItem)}>
+                &#10005;
+            </div>
+        </div>
+    )
+}
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item)),
+    clearItem: item => dispatch(clearItemFromCart(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem)
